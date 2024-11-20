8f1996c6878b9bb94c14549bc09e18719d6bfa25
dev-gursewak <gursewak@duck.com>
2024-11-20 15:22:59 -0700

    can generate given no of square clear the cell as well

--- myscript.js
+++ myscript.js
@@ -1,11 +1,34 @@
 const container = document.getElementById('container');
 
-for (let i=0; i<16*16; i++){
-    let div = document.createElement("div");
-    div.style.border = "1px solid black";
-    div.style.margin = ".01px";
-    div.style.padding = "56px";
-    div.style.flexGrow = "1";
-    container.appendChild(div);
+function requiredSquare() {
+    const girdSize = prompt("required Gird Size: ");
+    if (girdSize > 100) {
+        alert("Please enter smaller number. It should be less than 100.");
+        container.innerHTML = ""
+    } else {
+        gridGenerator(girdSize)
+    }
+}
+function gridGenerator(e) {
+    container.innerHTML = "";
+    for (let i = 0; i < e * e; i++) {
+        let div = document.createElement("div");
+        div.style.border = "1px solid black";
+        div.style.margin = ".01px";
+        div.style.padding = "56px";
+        div.style.flexGrow = "1";
+        div.classList.toggle('child')
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        })
+        container.appendChild(div);
+
+    }
 }
 
+function clearCell(){
+    const child = document.querySelectorAll('.child');
+    child.forEach(child => {
+        child.style.backgroundColor = "white";
+    })
+}
