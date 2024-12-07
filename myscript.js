e43b868657fe6ad69bd5e83cfb54ba7ce732d8e6
dev-gursewak <gursewak@duck.com>
2024-12-07 13:42:41 -0700

    from avd

--- myscript.js
+++ myscript.js
@@ -11,19 +11,24 @@ function requiredSquare() {
 }
 function gridGenerator(e) {
     container.innerHTML = "";
-    for (let i = 0; i < e * e; i++) {
+    let parentDiv = document.createElement("div");
+    parentDiv.classList.toggle('parent');
+    for (let i = 0; i < e; i++) {
         let div = document.createElement("div");
         div.style.border = "1px solid black";
         div.style.margin = ".01px";
-        div.style.padding = "56px";
         div.style.flexGrow = "1";
         div.classList.toggle('child')
+        div.style.flex = "1 1 auto";
+        div.style.alignSelf = "stretch";
         div.addEventListener("mouseover", () => {
             div.style.backgroundColor = "black";
         })
-        container.appendChild(div);
-
+        parentDiv.appendChild(div);
     }
+    parentDiv.style.display = "flex";
+    parentDiv.style.flexDirection = "row";
+    container.appendChild(parentDiv);
 }
 
 function clearCell(){
